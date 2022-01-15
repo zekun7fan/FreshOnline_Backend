62e5ee25ce6e1e8530f3715354293aa0ebe2dea0
ZEKUN FAN <z62fan@uwaterloo.ca>
2022-01-14 20:16:18 -0500

    set up freshonline backend

---
+++ src/main/java/com/example/freshonline/FreshOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.freshonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/freshonline/FreshOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freshonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
