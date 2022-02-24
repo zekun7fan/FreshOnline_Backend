dde09711a3819fcb95a3ed010a8f484e86c1f7da
Josh <sunqixuanno1@126.com>
2022-02-23 22:41:23 -0500

    add dao stockedgoodsmapper test

--- src/test/java/com/example/freshonline/dao/StockedGoodsMapperTests.java
+++ src/test/java/com/example/freshonline/dao/StockedGoodsMapperTests.java
@@ -1,11 +1,16 @@
 package com.example.freshonline.dao;
 
 import com.example.freshonline.FreshOnlineApplication;
+import com.example.freshonline.dto.SearchParams;
+import com.example.freshonline.dto.SearchResultInfo;
+import com.example.freshonline.model.StockedGoods;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,9 +23,23 @@ class StockedGoodsMapperTests {
 
     @Test
     public void testSelectByFilter() {
+        SearchParams param = new SearchParams();
+        param.setCategory_id("(123,124)");
+        param.setPage(1);
+        param.setBrands("('tnt','abc')");
+        List<StockedGoods> goods = stockedGoodsMapper.selectByFilter(param);
+        for (StockedGoods good : goods){
+            System.out.println(good);
+        }
     }
 
     @Test
     public void testSearchInfo() {
+        SearchParams param = new SearchParams();
+        param.setCategory_id("(123,124)");
+        param.setPage(1);
+        param.setBrands("('tnt','abc')");
+        SearchResultInfo info = stockedGoodsMapper.searchInfo(param);
+        System.out.println(info);
     }
 }
