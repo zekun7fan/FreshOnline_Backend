0b13f3628a5cf7e740720e2814cdc0902cf895be
Zekun Fan <z62fan@uwaterloo.ca>
2022-02-27 14:41:45 -0500

    simplify search params dto

--- src/main/java/com/example/freshonline/dto/SearchParams.java
+++ src/main/java/com/example/freshonline/dto/SearchParams.java
@@ -2,102 +2,81 @@ package com.example.freshonline.dto;
 
 
 import com.example.freshonline.utils.ValidationChecker;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Map;
+import java.util.Objects;
 
-
+@Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class SearchParams implements VerifyRequestData<SearchParams> {
 
-    @Getter @Setter
-    public int price_low = -1;
-    @Getter @Setter
-    public int price_high = -1;
-    @Getter @Setter
-    public String brands;
-    @Getter @Setter
-    public int sort_type = -1;
-    @Getter @Setter
-    public String keyword;
-    @Getter @Setter
-    public int page = 1;
-    @Getter @Setter
-    public String category_id;
-    @Getter @Setter
-    public int num_per_row = 4, row_per_page = 5;
-    @Setter
-    public int item_low = -1;
-    @Setter
-    public int item_high = -1;
+    private int price_low = -1;
+    private int price_high = -1;
+    private String brands = null;
+    private int sort_type = -1;
+    private String keyword = null;
+    private int page = 1;
+    private String category_id = null;
+    private int num_per_row = 4, row_per_page = 5;
+    private int item_low = -1;
+    private int item_high = -1;
 
     @Override
-    public SearchParams verifyParams(Map<String, String> param){
-        SearchParams output = new SearchParams();
-//        ValidationChecker vc = new ValidationChecker();
-        try{
-            if (param.containsKey("page")){
-                int tmpPage = Integer.parseInt(param.get("page"));
-                output.setPage(tmpPage > 0 ? tmpPage: 1);
-            }
-            if (param.containsKey("price_low")) {
-                output.setPrice_low(Integer.parseInt(param.get("price_low")));
-            }
-            if (param.containsKey("price_high")) {
-                output.setPrice_high(Integer.parseInt(param.get("price_high")));
-            }
-            if (param.containsKey("sort_type")) {
-                output.setSort_type(Integer.parseInt(param.get("sort_type")));
-            }
-            if (param.containsKey("num_per_row")) {
-                output.setNum_per_row(Integer.parseInt(param.get("num_per_row")));
-            }
-            if (param.containsKey("row_per_page")) {
-                output.setRow_per_page(Integer.parseInt(param.get("row_per_page")));
-            }
-        } catch (NumberFormatException e){
-            System.out.println("Searchparam, NumberFormatException");
-            System.out.println(e.getMessage());
-            throw e;
+    public SearchParams verifyParams(Map<String, String> param) {
+        try {
+            int tmpPage = Integer.parseInt(param.get("page"));
+            this.setPage(tmpPage > 0 ? tmpPage : 1);
+        } catch (NumberFormatException ignored) {
         }
-        if (param.containsKey("brands")) {
-            String brandsStr = param.get("brands");
-            if (brandsStr==null || brandsStr.isBlank()) {
-                output.setBrands(null);
-            } else {
-                try{
-                    String[] brands_list = param.get("brands").split(",");
-                    output.setBrands("('" + String.join("','", brands_list) + "')");
-                } catch (Exception e) {output.setBrands(null);}
-            }
+        try {
+            this.setPrice_low(Integer.parseInt(param.get("price_low")));
+        } catch (NumberFormatException ignored) {
         }
-        if (param.containsKey("keyword")) {
-            if (param.get("keyword") != null && param.get("keyword").length() > 0) {
-                output.setKeyword(param.get("keyword"));
-            } else { output.setKeyword(null); }
+        try {
+            this.setPrice_high(Integer.parseInt(param.get("price_high")));
+        } catch (NumberFormatException ignored) {
         }
-        if (param.containsKey("category_id")) {
-            if (param.get("category_id") != null && param.get("category_id").length() > 0){
-                try {
-                    String[] category_id_list = param.get("category_id").split(",");
-                    output.setCategory_id("('" + String.join("','", category_id_list) + "')");
-                } catch (Exception e) {output.setCategory_id(null);}
-            } else { output.setCategory_id(null); }
+        try {
+            this.setSort_type(Integer.parseInt(param.get("sort_type")));
+        } catch (NumberFormatException ignored) {
         }
-        output.computePage();
-        return output;
+        try {
+            this.setNum_per_row(Integer.parseInt(param.get("num_per_row")));
+        } catch (NumberFormatException ignored) {
+        }
+        try {
+            this.setRow_per_page(Integer.parseInt(param.get("row_per_page")));
+        } catch (NumberFormatException ignored) {
+        }
+
+        String brandsStr = param.get("brands");
+        if (!Objects.requireNonNull(brandsStr).isBlank()) {
+            String[] brands_list = param.get("brands").split(",");
+            this.setBrands("('" + String.join("','", brands_list) + "')");
+        }
+        String keywordStr = param.get("keyword");
+        if (!Objects.requireNonNull(keywordStr).isBlank()) {
+            this.setKeyword(keywordStr);
+        }
+        String categoryIdStr = param.get("category_id");
+        if (categoryIdStr != null) {
+            String[] category_id_list = categoryIdStr.split(",");
+            this.setCategory_id("('" + String.join("','", category_id_list) + "')");
+        }
+        this.computePage();
+        return this;
     }
 
-    private void computePage(){
+    private void computePage() {
         this.setItem_low((this.getPage() - 1) * this.getNum_per_row() * this.getRow_per_page());
         this.setItem_high(this.getPage() * this.getNum_per_row() * this.getRow_per_page());
     }
 
     @Override
-    public String toString(){
-        return  " category_id = " + category_id + " keyword = " + keyword
+    public String toString() {
+        return " category_id = " + category_id + " keyword = " + keyword
                 + "\n selected brands = " + brands + " price_low = " + price_low + " price_high = " + price_high + " sort_type = " + sort_type
                 + "\n page = " + page + " item_low = " + item_low + " item_high = " + item_high;
     }
