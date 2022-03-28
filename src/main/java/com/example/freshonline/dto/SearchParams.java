508999d2e9ae8a9ceb5760765e7f7738243b80cf
Josh <sunqixuanno1@126.com>
2022-03-27 20:16:40 -0400

    nullable

--- src/main/java/com/example/freshonline/dto/SearchParams.java
+++ src/main/java/com/example/freshonline/dto/SearchParams.java
@@ -12,19 +12,19 @@ import java.util.List;
 @NoArgsConstructor
 public class SearchParams implements VerifyRequestData {
 
-    @NotNull @PositiveOrZero
+    @PositiveOrZero
     private int price_low = 0;
-    @NotNull @PositiveOrZero
+    @PositiveOrZero
     private int price_high = 0;
-    @NotNull
+
     private List<String> brands = null;
-    @NotNull @Min(0) @Max(4)
+    @Min(0) @Max(4)
     private int sort_type = 0;
-    @NotNull
+
     private String keyword = null;
-    @NotNull @Positive
+    @Positive
     private int page = 1;
-    @NotNull
+
     private List<Integer> category_id = null;
 
 
