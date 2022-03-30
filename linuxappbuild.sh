84a1ba8f5d4dcdbec73514c3f66a50be543deeb7
Zetian Huang <z437huang@uwaterloo.ca>
2022-03-30 05:35:16 +0000

    Update linuxappbuild.sh

--- linuxappbuild.sh
+++ linuxappbuild.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 docker stop freshonline
 docker rm freshonline
-./mvnw package
 docker build -t springio/gs-spring-boot-docker .
 docker run -p 8080:8080 --name freshonline springio/gs-spring-boot-docker -d
-
-
