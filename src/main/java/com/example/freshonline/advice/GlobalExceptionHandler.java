407df55148080df832ea9720dbea1dd0ae084544
Zetian Huang <z437huang@uwaterloo.ca>
2022-04-03 18:07:25 +0000

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/freshonline/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/freshonline/advice/GlobalExceptionHandler.java
@@ -43,18 +43,5 @@ public class GlobalExceptionHandler {
             ex.printStackTrace();
         }
     }
-
-    @ExceptionHandler(ConnectException.class)
-    void handle(ConnectException e, HttpServletRequest request, HttpServletResponse response) {
-        response.setStatus(Response.SC_OK);
-        try {
-            PrintWriter writer = response.getWriter();
-            String resp = JSONObject.toJSONString(RespBuilder.createFailedRsp("Network Exception!"));
-            writer.write(resp);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
     
 }
