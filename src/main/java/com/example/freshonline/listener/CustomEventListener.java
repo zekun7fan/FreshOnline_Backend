39e6fae55bd488bdd69959130adae8b77656dce5
Jerry Xu <j572xu@uwaterloo.ca>
2022-04-03 14:38:11 -0400

    debug complete remove printlns

--- src/main/java/com/example/freshonline/listener/CustomEventListener.java
+++ src/main/java/com/example/freshonline/listener/CustomEventListener.java
@@ -44,24 +44,19 @@ public class CustomEventListener {
             @Override
             public void run() {
                 CreateOrderDetail detail = event.getDetail();
-                System.out.println("break1");
 
                 // create order
                 Integer orderId = orderService.createOrder(detail.getUserId(), detail.getLocation());
-                System.out.println("break2");
 
                 // record saled goods
                 saledGoodsService.addSaledGoods(orderId, detail.getOrderedGoods());
-                System.out.println("break3");
 
                 // delete goods from cart
                 ArrayList<Integer> goodsIdList = new ArrayList<>(detail.getOrderedGoods().keySet());
-                System.out.println("break4");
 
                 if (goodsIdList.size() > 0){
                     cartService.batchDeleteFromCart(detail.getUserId(), goodsIdList);
                 }
-                System.out.println("break5");
             }
         });
     }
